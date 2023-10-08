1a6c9f86933a4758292ce916f9d492a2c7527c8c
kozarovdusan <kozarovdusan@uns.ac.rs>
2023-10-08 17:42:10 +0200

    Added controllers for fuel log and vehicles

---
+++ src/FuelLog/Controllers/FuelLogController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FuelLog.Controllers
+{
+    public class FuelLogController : Controller
+    {
+        public IActionResult Index()
+        {
+            public IActionResult AddLog()
+            {
+                if (GetById(Id) != FuelLog)
+                {
+                    return BadRequest();
+                }
+
+                AddLog(FuelLog);
+
+                return Ok();
+            }
+
+            public IActionResult GetAll()
+            {
+                if (GetAll == null)
+                {
+                    return NotFound();
+                }
+                return Ok(GetAll());
+            }
+
+            public IActionResult GetByVehicle()
+            {
+                if (GetByVehicle == null)
+                {
+                    return NotFound();
+                }
+                return Ok(GetByVehicle());
+            }
+
+            public IActionResult GetById()
+            {
+                if (GetById(Id) == null)
+                {
+                    return NotFound();
+                }
+                return Ok(GetById());
+            }
+
+            public IActionResult UpdateLog()
+            {
+                if (GetType(GetById(Id)) != FuelLog)
+                {
+                    return BadRequest();
+                }
+
+                UpdateLog(FuelLog);
+
+                return Ok();
+            }
+
+            public IActionResult DeleteLog()
+            {
+                if (GetById(Id) == null)
+                {
+                    return NotFound();
+                }
+
+                DeleteLog(id);
+
+                return Ok();
+            }
+
+            return View();
+        }
+    }
+}
---
+++ src/FuelLog/Controllers/VehicleController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FuelLog.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            public IActionResult AddVehicle()
+            {
+                if (GetType(GetVehicle(Id)) != Vehicle)
+                {
+                    return BadRequest();
+                }
+
+                AddVehicle(Vehicle);
+
+                return Ok();
+            }
+
+            public IActionResult GetAll()
+            {
+                if (GetAll == null)
+                {
+                    return NotFound();
+                }
+                return Ok(GetAll());
+            }
+
+            public IActionResult GetVehicle()
+            {
+                if (GetVehicle == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(GetVehicle());
+            }
+
+            public IActionResult UpdateVehicle()
+            {
+                if (GetVehicle(Id) != Vehicle)
+                {
+                    return BadRequest();
+                }
+
+                UpdateVehicle(Vehicle);
+
+                return Ok();
+            }
+
+            public IActionResult DeleteVehicle()
+            {
+                if (GetVehicle == null)
+                {
+                    return NotFound();
+                }
+
+                DeleteVehicle(Vehicle);
+
+                return Ok();
+            }
+
+            return View();
+        }
+    }
+}
