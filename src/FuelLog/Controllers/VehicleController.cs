c311048990e1ee9c4367cc7c634a3a22caedd381
KoAleksandar <112872470+KoAleksandar@users.noreply.github.com>
2023-10-10 14:51:12 +0200

    Update VehicleController.cs

--- src/FuelLog/Controllers/VehicleController.cs
+++ src/FuelLog/Controllers/VehicleController.cs
@@ -4,64 +4,58 @@ namespace FuelLog.Controllers
 {
     public class VehicleController : Controller
     {
-        public IActionResult Index()
+        public IActionResult AddVehicle()
         {
-            public IActionResult AddVehicle()
+            if (GetType(GetVehicle(Id)) != Vehicle)
             {
-                if (GetType(GetVehicle(Id)) != Vehicle)
-                {
-                    return BadRequest();
-                }
-
-                AddVehicle(Vehicle);
-
-                return Ok();
+                return BadRequest();
             }
 
-            public IActionResult GetAll()
-            {
-                if (GetAll == null)
-                {
-                    return NotFound();
-                }
-                return Ok(GetAll());
-            }
-
-            public IActionResult GetVehicle()
-            {
-                if (GetVehicle == null)
-                {
-                    return NotFound();
-                }
-
-                return Ok(GetVehicle());
-            }
-
-            public IActionResult UpdateVehicle()
-            {
-                if (GetVehicle(Id) != Vehicle)
-                {
-                    return BadRequest();
-                }
-
-                UpdateVehicle(Vehicle);
-
-                return Ok();
-            }
-
-            public IActionResult DeleteVehicle()
-            {
-                if (GetVehicle == null)
-                {
-                    return NotFound();
-                }
-
-                DeleteVehicle(Vehicle);
-
-                return Ok();
-            }
-
-            return View();
+            AddVehicle(Vehicle);
+            return Ok();
         }
+
+        public IActionResult GetAll()
+        {
+            if (GetAll == null)
+            {
+                return NotFound();
+            }
+            return Ok(GetAll());
+        }
+
+        public IActionResult GetVehicle()
+        {
+            if (GetVehicle == null)
+            {
+                return NotFound();
+            }
+            return Ok(GetVehicle());
+        }
+
+        public IActionResult UpdateVehicle()
+        {
+            if (GetVehicle(Id) != Vehicle)
+            {
+                return BadRequest();
+            }
+            UpdateVehicle(Vehicle);
+
+            return Ok();
+        }
+
+        public IActionResult DeleteVehicle()
+        {
+            if (GetVehicle == null)
+            {
+                return NotFound();
+            }
+
+            DeleteVehicle(Vehicle);
+
+            return Ok();
+        }
+        
+        return View();
     }
 }
