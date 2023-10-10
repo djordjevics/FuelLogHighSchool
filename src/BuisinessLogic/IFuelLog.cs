2d040a233a39c397878438bce1808cbef84fe05e
Filip Zecevic <filip.zecevic@jjzmaj.edu.rs>
2023-10-10 15:14:01 +0200

    Split intefaces into separate files.

---
+++ src/BuisinessLogic/IFuelLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisinessLogic
+{
+    public interface IFuelLog
+    {
+        public interface IFuelLog
+        {
+            IEnumerable<FuelLog> GetAllFuelLogs();
+            IEnumerable<IFuelLog> GetAllFuelLogsByVehicleId(int id);
+            FuelLog GetFuelLogById(int id);
+            void AddFuelLogToVehicle(int id, FuelLog fuel_log);
+            void UpdateFuelLog(int id, FuelLog fuel_log);
+            void DeleteFuelLog(int id);
+        }
+    }
+}
