1debbdae393031c57a05ce190d8c959744400ff9
injacmarko <marko.injac05@gmail.com>
2023-10-11 14:29:45 +0200

    Removed unused usings

--- src/BuisinessLogic/Models/Enums/FuelType.cs
+++ src/BuisinessLogic/Models/Enums/FuelType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BuisinessLogic.Models.Enums
+﻿namespace BuisinessLogic.Models.Enums
 {
     public enum FuelType
     {
