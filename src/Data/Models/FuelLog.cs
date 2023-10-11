8a3cc78a53c9bd1c724f4abb74308f7f9317c365
injacmarko <111227009+injacmarko@users.noreply.github.com>
2023-10-11 16:53:51 +0200

    Created Data Models (#17)

---
+++ src/Data/Models/FuelLog.cs
@@ -0,0 +1,11 @@
+namespace Data.Models
+{
+    public class FuelLog
+    {
+        public int Id;
+        public int Odometer;
+        public int AmountFilled;
+        public int Cost;
+        public int TruckId;
+    }
+}
---
+++ src/Data/Models/Vehicle.cs
@@ -0,0 +1,12 @@
+namespace Data.Models
+{
+    public class Vehicle
+    {
+        public int Id;
+        public string Model;
+        public string LicensePlate;
+        public string Color;
+        public string FuelType;
+        public int WheelCount;
+    }
+}
