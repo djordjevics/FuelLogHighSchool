fc9063211cf9f9a0e6e25d81cb62a793995e27e3
injacmarko <marko.injac05@gmail.com>
2023-10-16 10:07:24 +0200

    Data mapper WIP

---
+++ src/BuisinessLogic/Mappers/BusinessToData.cs
@@ -0,0 +1,19 @@
+﻿using BuisinessLogic.Models.Enums;
+using BusinessLogic.Models;
+using Data.Models;
+
+namespace BuisinessLogic.Mappers
+{
+    public class BusinessToData
+    {
+        public FuelLogData toFuellogData(FuelLogDTO fuelLogDTO)
+        {
+            return new FuelLogData
+            {
+                Id = fuelLogDTO.Id,
+                Odometer = fuelLogDTO.OdometerUnit == OdometerUnit.MI ? Converter.MiToKm(fuelLogDTO.OdometerUnit) : fuelLogDTO.OdometerUnit,
+                AmountFilled = fuelLogDTO.AmountFilledUnit == AmountFilledUnit.IMPGALLONS ? Converter.
+            };
+        }
+    }
+}
