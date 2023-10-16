655e2f5da5c9aff7e1acdd8fe8a30017c4627b0b
injacmarko <marko.injac05@gmail.com>
2023-10-16 09:59:10 +0200

    Business to api

--- src/BuisinessLogic/Mappers/BusinessToAPI.cs
+++ src/BuisinessLogic/Mappers/BusinessToAPI.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BusinessLogic.Models;
+using FuelLogAPI.Models;
+using FuelLogAPI.Models.Enums;
 
 namespace BuisinessLogic.Mappers
 {
-    internal class BusinessToAPI
+    public class BusinessToAPI
     {
+        public Vehicle toVehicle(VehicleDTO vehicleDTO)
+        {
+            return new Vehicle
+            {
+                Id = vehicleDTO.Id,
+                Model = vehicleDTO.Model,
+                Color = vehicleDTO.Color,
+                LicensePlate = vehicleDTO.LicensePlate,
+                FuelType = (FuelType)vehicleDTO.FuelType,
+                WheelCount = vehicleDTO.WheelCount
+            };
+        }
+
+        public FuelLog toFuelLog(FuelLogDTO fuelLogDTO)
+        {
+            return new FuelLog
+            {
+                Id = fuelLogDTO.Id,
+                AmountFilled = fuelLogDTO.AmountFilled,
+                AmountFilledUnit = (AmountFilledUnit)fuelLogDTO.AmountFilledUnit,
+                Odometer = fuelLogDTO.Odometer,
+                OdometerUnit = (OdometerUnit)fuelLogDTO.OdometerUnit,
+                Cost = fuelLogDTO.Cost,
+                CostCurrency = (CostCurrency)fuelLogDTO.CostCurrency,
+                VehicleId = fuelLogDTO.VehicleId
+            };
+        }
     }
 }
