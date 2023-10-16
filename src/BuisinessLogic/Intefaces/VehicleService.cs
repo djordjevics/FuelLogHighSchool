aaf6a382333aa810afedd4fe7965ec774f3c2194
FilipZecevic <112871966+FilipZecevic@users.noreply.github.com>
2023-10-16 12:43:34 +0200

    Implemented all endpoints (#38)
    * Added new classes for inteface methods implementation and finnished all endpoints in VehicleController and FuelLogController
    
    * Implemented all endpoints fully and removed unnecessary enums

---
+++ src/BuisinessLogic/Intefaces/VehicleService.cs
@@ -0,0 +1,32 @@
+﻿using BuisinessLogic.Models;
+
+namespace BuisinessLogic.Intefaces
+{
+    public class VehicleService : IVehicleService
+    {
+        public void AddVehicle(CreateVehicle createVehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteVehicle(DeleteVehicleRequest deleteVehicleRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Vehicle> GetAllVehicles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vehicle GetVehicleById(GetVehicleById getVehicleById)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateVehicle(UpdateVehicleRequest updateVehicleRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
