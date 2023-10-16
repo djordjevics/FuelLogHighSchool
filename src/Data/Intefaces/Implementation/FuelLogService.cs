4edb4db4aaa96e4abb4b11982e53871e6a505bcd
Filip Zecevic <filip.zecevic@jjzmaj.edu.rs>
2023-10-16 12:35:17 +0200

    Added classes to Data that implement interfaces from Data

---
+++ src/Data/Intefaces/Implementation/FuelLogService.cs
@@ -0,0 +1,37 @@
+﻿using Data.Models;
+
+namespace Data.Intefaces.Implementation
+{
+    public class FuelLogService : IFuelLogService
+    {
+        public void AddFuelLogToVehicle(FuelLogData fuelLog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFuelLog(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FuelLogData> GetAllFuelLogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FuelLogData> GetAllFuelLogsByVehicleId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FuelLogData GetFuelLogById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFuelLog(FuelLogData fuelLog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
