d161bda915c0e50a855e8efd0bb09a5404e95303
FilipZecevic <112871966+FilipZecevic@users.noreply.github.com>
2023-10-16 12:39:41 +0200

    Added classes to Data that implement interfaces from Data (#42)

---
+++ src/Data/Intefaces/Implementation/VehicleService.cs
@@ -0,0 +1,32 @@
+﻿using Data.Models;
+
+namespace Data.Intefaces.Implementation
+{
+    public class VehicleService : IVehicleService
+    {
+        public void AddVehicle(VehicleData vehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteVehicle(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<VehicleData> GetAllVehicles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public VehicleData GetVehicleById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateVehicle(VehicleData vehicle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
