4a4410de004f617b86337dcdef7982731e7dc65d
injacmarko <marko.injac05@gmail.com>
2023-10-16 09:55:43 +0200

    API to business mapper

---
+++ src/BuisinessLogic/Mappers/APIToBusiness.cs
@@ -0,0 +1,61 @@
+﻿using BuisinessLogic.Models.Enums;
+using BusinessLogic.Models;
+using FuelLogAPI.Models;
+
+namespace BuisinessLogic.Mappers
+{
+    public class APIToBusiness
+    {
+        public VehicleDTO toVehicleDTO(CreateVehicle createVehicle)
+        {
+            return new VehicleDTO
+            {
+                Color = createVehicle.Color,
+                Model = createVehicle.Model,
+                LicensePlate = createVehicle.LicensePlate,
+                WheelCount = createVehicle.WheelCount,
+                FuelType = (FuelType)createVehicle.FuelType
+            };
+        }
+
+        public VehicleDTO toVehicleDTO(UpdateVehicleRequest updateVehicleRequest)
+        {
+            return new VehicleDTO
+            {
+                Color = updateVehicleRequest.UpdatedVehicle.Color,
+                Model = updateVehicleRequest.UpdatedVehicle.Model,
+                LicensePlate = updateVehicleRequest.UpdatedVehicle.LicensePlate,
+                WheelCount = updateVehicleRequest.UpdatedVehicle.WheelCount,
+                FuelType = (FuelType)updateVehicleRequest.UpdatedVehicle.FuelType
+            };
+        }
+
+        public FuelLogDTO toFuelLogDTO(CreateFuelLog createFuelLog)
+        {
+            return new FuelLogDTO
+            {
+                Odometer = createFuelLog.Odometer,
+                OdometerUnit = (OdometerUnit)createFuelLog.OdometerUnit,
+                AmountFilled = createFuelLog.AmountFilled,
+                AmountFilledUnit = (AmountFilledUnit)createFuelLog.AmountFilledUnit,
+                Cost = createFuelLog.Cost,
+                CostCurrency = (CostCurrency)createFuelLog.CostCurrency,
+                VehicleId = createFuelLog.VehicleId
+            };
+        }
+
+        public FuelLogDTO toFuelLogDTO(UpdateFuelLogRequest updateFuelLogRequest)
+        {
+            return new FuelLogDTO
+            {
+                Odometer = updateFuelLogRequest.UpdatedFuelLog.Odometer,
+                OdometerUnit = (OdometerUnit)updateFuelLogRequest.UpdatedFuelLog.OdometerUnit,
+                AmountFilled = updateFuelLogRequest.UpdatedFuelLog.AmountFilled,
+                AmountFilledUnit = (AmountFilledUnit)updateFuelLogRequest.UpdatedFuelLog.AmountFilledUnit,
+                Cost = updateFuelLogRequest.UpdatedFuelLog.Cost,
+                CostCurrency = (CostCurrency)updateFuelLogRequest.UpdatedFuelLog.CostCurrency,
+                VehicleId = updateFuelLogRequest.UpdatedFuelLog.VehicleId
+            };
+        }
+    }
+}
