1e36e3808162577cef59eca0c5705fd9f40a4428
injacmarko <111227009+injacmarko@users.noreply.github.com>
2023-10-20 11:08:57 +0200

    Fix avg fuel consumption (#56)
    * Fixed serialization
    
    * Cleanup
    
    * Simplifying
    
    * Fixed mapper issue where WheelCount field wouldnt be mapped to data model
    
    * Fixed ID not creating when adding items
    
    * All CRUD functions working
    
    * Removed json files
    
    * Fixed + implemented new feature
    
    * Implemented Get request for AvgFuelConsumption

--- src/BuisinessLogic/Interfaces/Implementations/VehicleService.cs
+++ src/BuisinessLogic/Interfaces/Implementations/VehicleService.cs
@@ -42,14 +42,34 @@ public class VehicleService : IVehicleService
         _vehicleServiceData.UpdateVehicle(updateVehicleRequest.ToVehicleDTO().ToVehicleData());
     }
 
-    public double AverageFuelConsumption(int vehicleId)
+    public double AvgFuelConsumption(int vehicleId)
     {
-        var logs = _fuelLogService.GetAllFuelLogsByVehicleId(new GetFuelLogsByVehicleId { VehicleId = vehicleId});
+        var logs = _fuelLogService.GetAllFuelLogsByVehicleId(new() { VehicleId = vehicleId});
 
-        double odoSum = logs.Sum(x => x.Odometer);
         double fuelConsumptedSum = logs.Sum(x => x.AmountFilled);
 
-        return odoSum / fuelConsumptedSum; 
+        return logs.Last().Odometer / fuelConsumptedSum; 
 
+    }
+
+    public double AvgFuelConsumptionBetweenFuelLogs(int vehicleId, int fuelLog1Id, int fuelLog2Id)
+    {
+        var log1 = _fuelLogService.GetFuelLogById(new() { Id = fuelLog1Id });
+        var log2 = _fuelLogService.GetFuelLogById(new() { Id = fuelLog2Id });
+
+        var logsFromVehicle = _fuelLogService.GetAllFuelLogsByVehicleId(new() { VehicleId = vehicleId });
+        List<FuelLog> logs = new();
+
+        for (int i = fuelLog1Id; i <= fuelLog2Id; i++)
+        {
+            if (logsFromVehicle.Single(x => x.Id == i) != null)
+            {
+                logs.Add(logsFromVehicle.Single(x => x.Id == i));
+            }
+        }
+
+        double fuelConsumptedSum = logs.Sum(x => x.AmountFilled);
+
+        return (log2.Odometer - log1.Odometer) / fuelConsumptedSum;
     }
 }
